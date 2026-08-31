8b7218e0e5794c8f110007e12a4358542173f749
developer-alpinetech <developer@alpinetech-india.com>
2026-08-31 23:53:05 +0530

    shainky push 2
    shainky push 2

--- hrms-frontend/next.config.js
+++ hrms-frontend/next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
 };
-
 
 module.exports = nextConfig;
